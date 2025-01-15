60abd3ff536601b9c093d13066afc5efe10d36f9
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-01-14 23:23:06 -0500

    Added: -> Price ascending and descending sorter

---
+++ src/main/java/com/waelsworld/productservice_proxy/services/sortingService/PriceHighToLowSorter.java
@@ -0,0 +1,13 @@
+package com.waelsworld.productservice_proxy.services.sortingService;
+
+
+import com.waelsworld.productservice_proxy.models.Product;
+
+import java.util.List;
+
+public class PriceHighToLowSorter implements Sorter {
+    @Override
+    public List<Product> sort(List<Product> products) {
+        return List.of();
+    }
+}
---
+++ src/main/java/com/waelsworld/productservice_proxy/services/sortingService/PriceLowToHighSorter.java
@@ -0,0 +1,12 @@
+package com.waelsworld.productservice_proxy.services.sortingService;
+
+import com.waelsworld.productservice_proxy.models.Product;
+
+import java.util.List;
+
+public class PriceLowToHighSorter implements Sorter {
+    @Override
+    public List<Product> sort(List<Product> products) {
+        return List.of();
+    }
+}
